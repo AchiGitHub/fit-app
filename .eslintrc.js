c44e84818be993fad74083f328c762267e2c9b9c
Inamul <mfihassan99@gmail.com>
2021-02-28 11:19:29 +0530

    new eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: 'react-native-wcandillon',
   rules: {
+    quotes: [2, 'single', {avoidEscape: true}],
     'prettier/prettier': [
       'error',
       {
